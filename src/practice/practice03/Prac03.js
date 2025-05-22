c34610ef8f5b2fee41a25a56e555cc92b9f91c8b
gagajooyo <gagajooyo@gmail.com>
2025-05-22 14:15:34 +0900

    연습3 컴포넌트화

---
+++ src/practice/practice03/Prac03.js
@@ -0,0 +1,15 @@
+
+function Prac03({text, textItem}) {
+
+    return(
+
+        <div className="Prac03">
+            <h1>제목 : {text}</h1>
+            <p>내용 :{textItem}</p>
+        </div>
+
+    ) 
+
+}
+
+export default Prac03;
---
+++ src/practice/practice03/Practice03.js
@@ -0,0 +1,51 @@
+import Prac03 from '//Prac03';
+
+function Practice03() {
+
+    let textArr = ['하나', '둘', '셋', '넷', '다섯']
+    let textItemArr = ['내용하나', '내용둘', '내용셋','내용넷','내용다섯']
+
+
+
+    return (
+        <div className="App">
+
+            
+
+                {/* <Prac03 text={textArr[0]} textItem={textItemArr[0]}/>
+                <Prac03 text={textArr[1]} textItem={textItemArr[1]}/>
+                <Prac03 text={textArr[2]} textItem={textItemArr[2]}/>
+                <Prac03 text={textArr[3]} textItem={textItemArr[3]}/>
+                <Prac03 text={textArr[4]} textItem={textItemArr[4]}/> */}
+
+
+            {/* <div className='textItem'>
+                <p className='title'>제목 : 하나</p>
+                <p>내용 : 내용하나</p>
+                </div>
+                <div className='textItem'>
+                <p className='title'>제목 : 둘</p>
+                <p>내용 : 내용둘</p>
+                </div>
+                <div className='textItem'>
+                <p className='title'>제목 : 셋</p>
+                <p>내용 : 내용셋</p>
+                </div>
+                <div className='textItem'>
+                <p className='title'>제목 : 넷</p>
+                <p>내용 : 내용넷</p>
+                </div>
+                <div className='textItem'>
+                <p className='title'>제목 : 다섯</p>
+                <p>내용 : 내용다섯</p>
+            </div> */}
+        </div>
+    );
+
+}
+
+
+
+
+
+export default Practice03;
