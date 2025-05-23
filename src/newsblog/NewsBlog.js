863361aa113129a47cb08960cf8fafa14f9cfcd2
gagajooyo <gagajooyo@gmail.com>
2025-05-23 14:07:12 +0900

    등록 기능 개선(빈값등록방지, 좋아요 갯수 정상작동)

--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -2,111 +2,139 @@ import './NewsBlog.css';
 import { useState } from 'react';
 import Modal from './Modal';
 
+function NewsBlog(){
 
-function NewsBlog () {
-    
-    let title = "React Study"
-    
-    let [news1, setNews1] = useState(['Today News','오늘의 뉴스']);
+    let title = "React Study";
 
+    let [news1, setNews1] = useState('오늘의 뉴스');
     let [news2, setNews2] = useState('어제의 뉴스');
     let [news3, setNews3] = useState('내일의 뉴스');
 
     let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
+                //                 ['Today News', '어제의 뉴스', '내일의 뉴스']
 
-    let [num, numSet] = useState(0);
-    let [num1, numSet1] = useState(0);
-    let [num2, numSet2] = useState(0);
+    let [likeCount1, setLikeCount1] = useState(0);
+    let [likeCount2, setLikeCount2] = useState(0);
+    let [likeCount3, setLikeCount3] = useState(0);
 
-    let [numArr, numSetArr] = useState( [0,0,0] );
-    
-    let [showModal, setshowModal] = useState(false);
+    let [likeCountArr, setLikeCountArr] = useState( [0, 0, 0] );
 
-    let [selectedTitle, setselectedTitle] = useState('');
-    
+    let [showModal, setShowModal] = useState(false);
+    //    0  1 
+
+    let [selectedTitle, setSelectedTitle] = useState('');
+    let [selectedLikeCount, setSelectedLikeCount] = useState(0);
+
+    //input 값을 저장할 변수
+    let [inputText, setInputText] = useState('');
+
     return (
-
         <div>
             <div className='black-nav'>
                 <h4>Blog Header</h4>
-                <p style={{color: "orange", fontSize: '20px'}}>{title}</p>
+                <p style={ {color:"orange", fontSize:'20px'} }>{title}</p>
             </div>
-            
-            <div className='post-list'>
-                {/* <h4>{news1}</h4> */}
 
-                <h4 onClick={()=>{
-                    if(showModal)
-                        setshowModal(false);
-                    else
-                        setshowModal(true);
-
-                        setselectedTitle(news[0]);
-
-                }}>{news[0]}<span onClick={()=>{
-                    let temp = [...numArr];
-                    temp[0] += 1;
-                    numSetArr(temp);
-                }} style={{color:'red'}}>❤</span>{numArr[0]}</h4>
-                <p>내용 무</p>          
-
-                {/*
-                <h4>{news[0]}<span onClick={()=>{
-                    numSet(num+1);
-                }}>❤</span>{num}</h4>
-                <p>내용 무</p>
+            <div>
+                <input type="text" id="input_news" value={inputText} onChange={(event)=>{
+                    //console.log(event);
+                    console.log(event.target.value);
+                    setInputText(event.target.value);
+                }}></input>
                 <button onClick={()=>{
-                    let temp = [...news];       // ... 임시변수에 완전복제 새로운 렌더링
-                    temp[0] = 'Today News';         // 
+                    //입력된 값을 제목 배열에 추가 
+                    //입력된 값 : input 요소에서 onChange 가 발생할때 -> inputText state변수에 저장되어 있음.
+                    //제목배열 news 배열변수에 추가 -> 리렌더링 -> news배열의 갯수만큼 반복(map) 화면에 표시
+
+                    //입력값이 있는 경우만~
+                    //if(inputText.trim() != ''){
+                    /*
+                    if(inputText.trim().length > 0){
+                        let temp = [...news];
+                        temp.push(inputText);
+                        setNews(temp);
+                        
+                        //배열state변수 바꾸는 법 : 배열 복사 -> 값 변경/추가/삭제 -> set함수로 원래 배열에 저장
+                        setInputText('');  //공백 -> input value={inputText}
+                    } else {
+                        alert("내용을 입력 후 등록하세요~");
+                        setInputText('');
+                    }*/
+
+                    //inputText = '    abc';
+                    
+                    //if(inputText.trim().length == 0){
+
+                    inputText = inputText.trim();
+                    if(inputText.length == 0){
+                        alert("내용을 입력 후 등록하세요~");
+                        setInputText('');
+                        return;
+                    }
+
+                    let temp = [...news];
+                    temp.push(inputText);
                     setNews(temp);
+
+                    likeCountArr.push(0);
                     
-                }}>제목 변경</button>
-                */}
-            </div>
-            <div className='post-list'>
-                {/* <h4>{news2}</h4> */}
-                <h4 onClick={()=>{
+                    //배열state변수 바꾸는 법 : 배열 복사 -> 값 변경/추가/삭제 -> set함수로 원래 배열에 저장
+                    setInputText('');  //공백 -> input value={inputText}
+
+
                     
-                        setshowModal(!showModal);
-                        setselectedTitle(news[1]);
+                    /*
+                    
+                    let title = document.getElementById('input_news').value;
+                    let temp = [...news];
+                    temp.push(title);
+                    setNews(temp);
 
-                }}>{news[1]}<span onClick={()=>{
-                    let temp = [...numArr];
-                    temp[1] += 1;
-                    numSetArr(temp);
-                }} style={{color:'red'}}>❤</span>{numArr[1]}</h4>
-                <p>내용 무</p>
+                    document.getElementById('input_news').value = '';
+
+                    */
+
+                }}>등록</button>
             </div>
 
-            <div className='post-list'>
-                {/* <h4>{news3}</h4> */}
-                <h4 onClick={()=>{
-                    if(showModal)
-                        setshowModal(false);
-                    else
-                        setshowModal(true);
-                        setselectedTitle(news[2]);
+            {
+                news.map((item, index)=>{   // 0 1 2 
+                    //return <div className='post-list'>
+                    return (
+                        <div className='post-list' key={index}>
+                            <h4 onClick={()=>{
+                                setShowModal(!showModal);
+                                setSelectedTitle(item);
+                                setSelectedLikeCount(likeCountArr[index]);
+                            }}>{item} <span onClick={(event)=>{
+                                
+                                event.stopPropagation();
+                                
+                                let temp = [...likeCountArr];
+                                temp[index] += 1;
+                                setLikeCountArr(temp);
 
-                }}>{news[2]}<span onClick={(event)=>{
-                    event.stopPropagation();        //Propagation 전파하다 이벤트 전파되는 것 막겠다.
-                    // 속해있는 부모 요소의 이벤트 처리까지 전파되는것을 막아줌.
-                    // <h4><span>h4안에 span 태그가 있으니 모두 속했는데</span></h4>
-                    let temp = [...numArr];
-                    temp[2] += 1;
-                    numSetArr(temp);
-                }} style={{color:'red'}}>❤</span>{numArr[2]}</h4>
-                <p>내용 무</p>
-                
-            </div>           
+                            }}>❤</span>{likeCountArr[index]}</h4>
+                            <p>내용 무</p>
+                        </div>
+                    )
 
-            {    
-                // showModal == true ? <Modal/> : null
-                showModal && <Modal title = {selectedTitle}/>
+                })
+            }
 
-            }    
+            {
+                //showModal == true ? <Modal/> : null
+                //props 전달 
+                // 변수, 배열, 객체, 함수, 요소(컴포넌트)
+                showModal && <Modal title={selectedTitle} likeCount={selectedLikeCount} 
+                                    news={news} setNews={setNews}
+                                    bgColor='lightblue'/>
+            }
             
         </div>
     );
 }
 
 export default NewsBlog;
+
+
